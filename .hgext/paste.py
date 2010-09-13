a5421562944c64b393cecacca6f4d198055de927
Daniel Graña <dangra@gmail.com>
2010-09-13 13:31:21 -0300

    update hg-paste

--- .hgext/paste.py
+++ .hgext/paste.py
@@ -1,96 +1,65 @@
 #!/usr/bin/env python
+'''Send diffs from Mercurial to various pastebin websites'''
 
-'''send diffs from Mercurial to various pastebin websites
-'''
-
-import base64
+import sys
 import urllib2
+from netrc import netrc
+from base64 import urlsafe_b64encode
 from mercurial import cmdutil, commands, help, util
 from urllib import urlencode
 
 
-def _paste_dpaste(content, **parameters):
-    data = {'content': content, 'language': 'Diff'}
-    if parameters['title']:
-        data['title'] = parameters['title']
-    if parameters['user']:
-        data['poster'] = parameters['user']
+def _paste_dpaste(content, syntax, title, user, keep, **kw):
+    return ('http://dpaste.com/api/v1/', {
+        'content': content, 
+        'language': syntax or 'Diff',
+        'title': title,
+        'poster': user[:30],
+        'hold': 'on' if keep else 'off',
+        })
 
-    if len(data['poster']) > 30:
-        data['poster'] = data['poster'][:30]
+def _paste_dpaste_org(content, syntax, title, user, keep, **kw):
+    return ('http://dpaste.org/', {
+        'content': content,
+        'lexer': syntax or 'diff',
+        'title': title,
+        'author': user[:30],
+        'expire_options': '3110400000' if keep else '2592000',
+        })
 
-    if parameters['keep']:
-        data['hold'] = 'on'
-    data = urlencode(data)
-    
-    if parameters['url']:
-        url = parameters['url']
-    else:
-        url = pastebins['dpaste']['url']
- 
-    request = urllib2.Request(url, data)
-    if parameters['httpauth']:
-        request.add_header('Authorization', 'Basic %s' \
-                    % base64.encodestring(parameters['httpauth'])[:-1])
-    elif parameters['usenetrc']:
-        auth = _netrc_get_credentials(url)
-        if auth:
-            request.add_header('Authorization', 'Basic %s' \
-                    % base64.encodestring('%s:%s' % (auth[0], auth[2]))[:-1])
+def _paste_pastebin_dixo_net(content, syntax, user, keep, **kw):
+    return ('http://pastebin.dixo.net/', {
+        'paste': 'Send',
+        'code2': content,
+        'format': syntax or 'diff',
+        'poster': user,
+        'expiry': 'f' if keep else 'm',
+        })
+
+def _paste_lodgeit(content, syntax, private, **kw):
+    data = {'code': content, 'language': syntax or 'diff'}
+    if private: data['private'] = 'on'
+    return ('http://paste.pocoo.org/', data)
+
+def _paste(handler, content, opts):
+    default_url, data = handler(content, **opts)
+    url = opts['url'] or default_url
+    request = urllib2.Request(url, urlencode(data, doseq=1))
+    _authorize_request(request, **opts)
     response = urllib2.urlopen(request)
     return response.geturl()
 
-def _paste_dpaste_org(content, **parameters):
-    data = {'content': content, 'lexer': 'diff'}
-    if parameters['title']:
-        data['title'] = parameters['title']
-    if parameters['user']:
-        data['author'] = parameters['user']
+def _authorize_request(request, httpauth=None, usenetrc=None, **kwargs):
+    """Sets basic authorization header using httpauth flag or netrc if enabled"""
+    if httpauth:
+        b64cred = urlsafe_b64encode(httpauth)
+        request.add_header('Authorization', 'Basic %s' % b64cred)
+    elif usenetrc:
+        cred = netrc().authenticators(request.get_host())
+        if cred:
+            b64cred = urlsafe_b64encode('%s:%s' % (cred[0], cred[2]))
+            request.add_header('Authorization', 'Basic %s' % b64cred)
 
-    if len(data['author']) > 30:
-        data['author'] = data['author'][:30]
-
-    # Same values used in dpaste.org form for default (a month) and forever expires.
-    data['expire_options'] = '3110400000' if parameters['keep'] else '2592000'
-    data = urlencode(data)
-
-    if parameters['url']:
-        url = parameters['url']
-    else:
-        url = pastebins['dpaste.org']['url']
-    
-    request = urllib2.Request(url, data)
-    if parameters['httpauth']:
-        request.add_header('Authorization', 'Basic %s' \
-                    % base64.encodestring(parameters['httpauth'])[:-1])
-    elif parameters['usenetrc']:
-        auth = _netrc_get_credentials(url)
-        if auth:
-            request.add_header('Authorization', 'Basic %s' \
-                    % base64.encodestring('%s:%s' % (auth[0], auth[2]))[:-1])
-    response = urllib2.urlopen(request)
-    return response.geturl()
-
-def _netrc_get_credentials(url):
-    import netrc, urlparse
-    credentials = netrc.netrc()
-    host = urlparse.urlparse(url).hostname
-    return credentials.authenticators(host)
-
-pastebins = {
-    'dpaste': { 'url': 'http://dpaste.com/api/v1/',
-                'parameters': {
-                    'required': ['content'],
-                    'optional': ['title', 'user', 'keep'], },
-                'handler': _paste_dpaste,
-    },
-    'dpaste.org': { 'url': 'http://dpaste.org/',
-                'parameters': {
-                    'required': ['content'],
-                    'optional': ['title', 'user', 'keep'], },
-                'handler': _paste_dpaste_org,
-    },
-}
 
 def paste(ui, repo, *fnames, **opts):
     '''send diffs from Mercurial to various pastebin websites
@@ -123,35 +92,43 @@ def paste(ui, repo, *fnames, **opts):
     dry = opts.pop('dry_run')
     if not dest:
         dest = 'dpaste'
-    if dest not in pastebins:
+
+    handler = globals().get('_paste_' + dest.replace('.', '_'))
+    if not handler:
         raise util.Abort('unknown pastebin (see "hg help pastebins")!')
-    
+
     if not opts['user']:
         opts['user'] = ui.username().replace('<', '').replace('>', '')
-    
-    ui.pushbuffer()
-    if opts['rev']:
-        rev = opts.pop('rev')
-        revs = cmdutil.revrange(repo, rev)
-        
-        if len(revs) == 1:
-            opts['change'] = revs[0]
-        else:
-            opts['rev'] = rev
-        
-        commands.diff(ui, repo, *fnames, **opts)
+
+    if opts['rev'] and opts['stdin']:
+        raise util.Abort('--rev and --stdin options are mutually exclusive')
+
+    if opts['stdin']:
+        content = sys.stdin.read()
     else:
-        commands.diff(ui, repo, *fnames, **opts)
-    content = ui.popbuffer()
-    
+        ui.pushbuffer()
+        if opts['rev']:
+            rev = opts.pop('rev')
+            revs = cmdutil.revrange(repo, rev)
+
+            if len(revs) == 1:
+                opts['change'] = revs[0]
+            else:
+                opts['rev'] = rev
+
+            commands.diff(ui, repo, *fnames, **opts)
+        else:
+            commands.diff(ui, repo, *fnames, **opts)
+        content = ui.popbuffer()
+
     if not content.strip():
         raise util.Abort('nothing to paste!')
-    
+
     if ui.verbose:
         ui.status('Pasting:\n%s\n' % content)
-    
+
     if not dry:
-        url = pastebins[dest]['handler'](content=content, **opts)
+        url = _paste(handler, content, opts)
         ui.write('%s\n' % url)
 
 
@@ -164,10 +141,14 @@ cmdtable = {
                            'username configured for Mercurial)'),
         ('k', 'keep', False, 'specify that the pastebin should keep the paste '
                              'for as long as possible (optional)'),
+        ('p', 'private', False, 'specify that the pastebin should mark a'
+                             'paste as private if possible (optional)'),
         ('',  'dry-run', False, 'do not paste to the pastebin'),
         ('',  'url', '', 'perform request against this url'),
         ('',  'httpauth', '', 'http authorization (user:pass)'),
         ('',  'usenetrc', False, 'use ~/.netrc for http authorization'),
+        ('',  'stdin', False, 'read content from standard input'),
+        ('',  'syntax', '', 'choose syntax'),
     ] + commands.diffopts + commands.walkopts,
     'hg paste [OPTION] [-r REV] [FILE...]')
 }
@@ -175,17 +156,22 @@ cmdtable = {
 help.helptable += (
     (['pastebins', 'pastebins'], ('Pastebins supported by hg-paste'),
      (r'''
-    hg-paste currently supports dpaste.com and dpaste.org.  More pastebins
-    will be supported in the future.
-    
     Available pastebins:
-    
+
     dpaste
         website: http://dpaste.com/
-        supported metadata options: --title, --keep, --user
+        supported metadata options: --title, --keep, --user --syntax
 
     dpaste.org
         website: http://dpaste.org/
-        supported metadata options: --title, --keep, --user
+        supported metadata options: --title, --keep, --user --syntax
+
+    pastebin.dixo.net
+        website: http://pastebin.dixo.net/
+        supported metadata options: --keep, --user --syntax
+
+    lodgeit
+        website: http://paste.pocoo.org/
+        supported metadata options: --syntax, --private
     ''')),
 )
