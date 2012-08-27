250013339f9d9bbab2264b329b54574f37a69078
Daniel Graña <dangra@gmail.com>
2012-08-27 13:58:56 -0300

    update xmonad conf to work with gnome3 panel

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 import XMonad
 import qualified XMonad.StackSet as W
 
@@ -21,13 +22,21 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Scratchpad
 import XMonad.Util.NamedScratchpad
 
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
+
 
 main :: IO ()
-main = xmonad $ gnomeConfig
+main = do
+    dbus <- D.connectSession
+    getWellKnownName dbus
+    xmonad $ gnomeConfig
       { terminal = myTerminal
       , modMask = mod4Mask
       , layoutHook = myLayouts
       , manageHook = myManageHook <+> manageHook gnomeConfig
+      , logHook = dynamicLogWithPP (prettyPrinter dbus)
       }
       `additionalKeysP` myKeys
 
@@ -78,4 +87,46 @@ myKeys =
     , ("M-|",          sendMessage $ ToggleStrut L)
     , ("M-n",          spawn "xrandr --output VGA1 --off --output LVDS1 --auto --primary")
     , ("M-m",          spawn "xrandr --output LVDS1 --off --output VGA1 --auto --primary")
+    , ("M-S-q",        spawn "gnome-session-quit")
     ]
+
+
+prettyPrinter :: D.Client -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput   = dbusOutput dbus
+    , ppTitle    = pangoSanitize
+    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
+    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
+    , ppHidden   = const ""
+    , ppUrgent   = pangoColor "red"
+    , ppLayout   = const ""
+    , ppSep      = " "
+    }
+
+getWellKnownName :: D.Client -> IO ()
+getWellKnownName dbus = do
+  D.requestName dbus (D.busName_ "org.xmonad.Log")
+                [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+  return ()
+
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
+            D.signalBody = [D.toVariant ("<b>" ++ (UTF8.decodeString str) ++ "</b>")]
+        }
+    D.emit dbus signal
+
+pangoColor :: String -> String -> String
+pangoColor fg = wrap left right
+  where
+    left  = "<span foreground=\"" ++ fg ++ "\">"
+    right = "</span>"
+
+pangoSanitize :: String -> String
+pangoSanitize = foldr sanitize ""
+  where
+    sanitize '>'  xs = "&gt;" ++ xs
+    sanitize '<'  xs = "&lt;" ++ xs
+    sanitize '\"' xs = "&quot;" ++ xs
+    sanitize '&'  xs = "&amp;" ++ xs
+    sanitize x    xs = x:xs
