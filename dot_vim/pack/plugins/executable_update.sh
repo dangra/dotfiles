079b89df027f7ed5720e5bf9f61d0f81d5ab0091
Daniel Graña <dangra@gmail.com>
2023-11-27 02:34:23 +0000

    sync

--- dot_vim/pack/plugins/executable_update.sh
+++ dot_vim/pack/plugins/executable_update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim:ft=sh:expandtab:ts=2
 PLUGINSDIR=$(dirname $(readlink -f $0))
 cd $PLUGINSDIR
 
@@ -7,28 +8,29 @@ dotgit() {
 }
 
 while read line; do
-  row=($line)
   # Skip empty lines and comments starting with "#"
-  [[ ! $row =~ ^\s*(#|$) ]] || continue
+  [[ ! $line =~ ^\s*(#|$) ]] || continue
+
   # Get remote url and git ref to checkout
+  row=($line)
   remote=${row[0]}
   gitref=${row[1]}
   clonedir=sources/$(basename $remote .git)
   echo "=> $remote (${gitref:-default})"
 
-  #mkdir -p $clonedir
-  # (
-  #   cd $clonedir
-  #   if [[ ! -d .git ]]; then
-  #     git init .
-  #     git remote add origin $remote
-  #   fi
-  #   git fetch origin --tags
-  #   git checkout -qf $gitref
-  #   git clean -dfq
-  # )
+  if [[ ! -d $clonedir ]]; then
+    git clone $remote $clonedir
+    [[ -z $gitref ]] || ( pushd $clonedir && git reset --hard $gitref )
+  elif [[ -n $gitref ]]; then
+    pushd $clonedir
+    git fetch
+    git reset --hard $gitref
+    popd
+  else
+    pushd $clonedir
+    git stash && git pull --rebase #&& git stash --apply
+    popd
+  fi
   ln -sf ../$clonedir start/
-  dotgit submodule add -f -- $remote $clonedir
-  #dotgit submodule init -- $clonedir
-  dotgit submodule update --remote -- $clonedir
+
 done <${1:-SOURCES.txt}
