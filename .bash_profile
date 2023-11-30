62ff2ef3068701d48bda2bcba09fba9ce9965b92
Daniel Graña <dangra@gmail.com>
2023-11-30 10:08:29 -0300

    sync with OSX

--- .bash_profile
+++ .bash_profile
@@ -2,4 +2,8 @@
 # ~/.bash_profile
 #
 
+[[ -r /usr/local/etc/profile.d/bash_completion.sh ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+[[ -f "/Applications/Tailscale.app/Contents/MacOS/Tailscale" ]] && alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
 [[ -f ~/.bashrc ]] && . ~/.bashrc
--- .config/nvim/config.lua
+++ .config/nvim/config.lua
@@ -6,6 +6,35 @@
 
 lvim.keys.normal_mode["<C-n>"] = ":bn<CR>"
 lvim.keys.normal_mode["<C-p>"] = ":bp<CR>"
-lvim.keys.normal_mode["se"] = ":Telescope fd<CR>"
+lvim.keys.normal_mode["se"] = ":tabedit "
 lvim.keys.normal_mode["sb"] = ":Telescope buffers<CR>"
 lvim.keys.normal_mode["sf"] = ":Telescope fd<CR>"
+
+lvim.plugins = {
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+}
+
+-- Run gofmt + goimport on save
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
+
