46ba9ab2f334b66cbc14468efd0d14c3f5c84bc1
Daniel Graña <dangra@gmail.com>
2025-01-28 10:35:30 -0300

    update avante config

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -43,20 +43,46 @@ return {
 
   -- nvim-cmp compatibility layer for Avante
   {
+    "saghen/blink.compat",
+    lazy = true,
+    opts = {},
+    config = function()
+      require("cmp").ConfirmBehavior = { Insert = "insert", Replace = "replace" }
+    end,
+  },
+  {
     "saghen/blink.cmp",
     lazy = true,
     opts = {
       sources = {
-        compat = { "avante_commands", "avante_mentions", "avante_files" },
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        compat = {
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
+        },
+        providers = {
+          -- LSP is typically ~60
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100, -- ~40 points higher than LSP ()
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000, -- show at a higher priority than lsp
+            opts = {},
+          },
+        },
       },
     },
-  },
-  {
-    "saghen/blink.compat",
-    opts = {},
-    lazy = true,
-    config = function()
-      require("cmp").ConfirmBehavior = { Insert = "insert", Replace = "replace" }
-    end,
   },
 }
