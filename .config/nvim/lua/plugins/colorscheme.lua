8dddbd4910c3ca03d1914e54bdfe2a4283851ce6
Daniel Graña <dangra@gmail.com>
2025-04-21 15:21:30 -0300

    monokai theme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+    },
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      day_night = {
+        enabled = true,
+      },
+    },
+  },
+}
