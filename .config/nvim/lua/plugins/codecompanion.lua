e31ed9d6e14f374fddb5a030999e813bf78e3613
Daniel Graña <dangra@gmail.com>
2025-04-24 11:53:50 -0300

    add codecompanion

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            name = "copilot",
+            schema = {
+              model = {
+                default = "claude-3.7-sonnet",
+              },
+            },
+          })
+        end,
+      },
+    },
+  },
+}
